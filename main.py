c35fd5375c84235599255eb06e6d04c605693e47
seungwon <69955858+tjtmddnjswkd@users.noreply.github.com>
2022-06-01 00:00:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2
 import time
 import tqdm
 import torch
+import random
 import numpy as np
 import pandas as pd
 import torch.nn as nn
@@ -19,13 +20,14 @@ from torch.utils.data.sampler import SubsetRandomSampler
 from torchsampler import ImbalancedDatasetSampler
 
 ap = argparse.ArgumentParser()
-ap.add_argument("--mode", required = True, type = str, help='IBOT or MOCO or DINO or BASE')
-ap.add_argument("--epochs", default = 25, type = int, help='number of training epochs')
-ap.add_argument("--batch_size", default = 32, type = int, help='training batchsize')
-# ap.add_argument("--save_path", required = True, type = str, help='path to storage the loss table, stat_dict, csv')
-ap.add_argument("--lr", default = 5e-5, type = float, help='learning rate , default 5e-5')
-ap.add_argument("--wd", default = 5e-5, type = float, help='weight decay, default 5e-3')
-ap.add_argument("--SAM", default = False, type = bool, help='using SAM')
+ap.add_argument("--mode", required=True, type=str, help='IBOT or MOCO or DINO or BASE')
+ap.add_argument("--epochs", default=25, type=int, help='number of training epochs')
+ap.add_argument("--batch_size", default=32, type=int, help='training batchsize')
+# ap.add_argument("--save_path", required=True, type=str, help='path to storage the loss table, stat_dict, csv')
+ap.add_argument("--lr", default=5e-5, type=float, help='learning rate , default 5e-5')
+ap.add_argument("--wd", default=1e-2, type=float, help='weight decay, default 5e-3')
+ap.add_argument("--SAM", default=False, type=bool, help='using SAM')
+ap.add_argument("--FL", default=False, type=bool, help='using focal loss')
 args = vars(ap.parse_args())
 
 class SAM(torch.optim.Optimizer):
@@ -84,6 +86,14 @@ class SAM(torch.optim.Optimizer):
                )
         return norm
 
+def seed_torch(seed=42):
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+
 def score_function(real, pred):
     score = f1_score(real, pred, average="macro")
     return score
@@ -117,69 +127,63 @@ def get_mean_std(data_dir):
 def create_datasets(batch_size):
     
     train_transform = transforms.Compose([
-    # transforms.RandomHorizontalFlip(),  # 좌우반전 
-    # transforms.RandomVerticalFlip(),  # 상하반전 
-    transforms.Resize((512, 512)),  # 알맞게 변경하세요 
+    transforms.Resize((512, 512)),
     transforms.ToTensor(),  # 이 과정에서 [0, 255]의 범위를 갖는 값들을 [0.0, 1.0]으로 정규화, torch.FloatTensor로 변환
-    transforms.Normalize([0.43336138, 0.4037862, 0.39447215], [0.18141639, 0.1738924, 0.16310532])  #  정규화(normalization)
+    transforms.Normalize([0.43322375, 0.40371937, 0.39443374], [0.18148199, 0.17403568, 0.16333582])  #  정규화(normalization)
     ])
     
-
     # choose the training and test datasets
-    train_data = datasets.ImageFolder(os.path.join('./train'), train_transform)
+    train_data = datasets.ImageFolder(os.path.join('/data/mvtec_ad_train'), train_transform)
+    test_data = datasets.ImageFolder(os.path.join('/data/mvtec_ad_test'), train_transform)
     label = [i[1] for i in train_data.samples]
-    # import pdb;pdb.set_trace()
-    test_data = TestDataset('./open/test')
-
+    
     # trainning set 중 validation 데이터로 사용할 비율
-    # valid_size = 0.2
+    valid_size = 0.2
 
     # validation으로 사용할 trainning indices를 얻는다.
-    # num_train = len(train_data)
-    # indices = list(range(num_train))
-    # train_idx, valid_idx, _, _ = train_test_split(indices, label, test_size=0.2, random_state=1004, shuffle=True, stratify=label)
+    num_train = len(train_data)
+    indices = list(range(num_train))
+    train_idx, valid_idx, _, _ = train_test_split(indices, label, test_size=0.2, random_state=42, shuffle=True, stratify=label)
     
     
     # trainning, validation batch를 얻기 위한 sampler정의
-    # train_sampler = SubsetRandomSampler(train_idx)
-    # valid_sampler = SubsetRandomSampler(valid_idx)
+    train_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
 
     # load training data in batches
     train_loader = torch.utils.data.DataLoader(train_data,
                                                batch_size=batch_size,
-                                               shuffle=True,
+                                               sampler=train_sampler,
                                                num_workers=4)
-    # train_loader = torch.utils.data.DataLoader(train_data,
-    #                                         sampler=ImbalancedDatasetSampler(train_data),
-    #                                         batch_size=batch_size,
-    #                                         num_workers=4)
     # load validation data in batches
     valid_loader = torch.utils.data.DataLoader(train_data,
                                                batch_size=batch_size,
+                                               sampler=valid_sampler,
                                                num_workers=4)
-    # valid_loader = None
-
+    
     # load test data in batches
-    test_loader = torch.utils.data.DataLoader(test_data, shuffle=False, batch_size=batch_size)
+    test_loader = torch.utils.data.DataLoader(test_data,
+                                              shuffle=False, 
+                                              batch_size=batch_size)
 
     return train_data, train_loader, valid_loader, test_loader
 
 def main():
     
     ## mean, std 계산하기
-    # data_dir = "train"
+    # data_dir = "./train_images"
     # get_mean_std(data_dir)
-    # data_dir = "./"
-    # import pdb;pdb.set_trace()
     # 512 x 512 일 때
-    # 평균 0.43336138 0.4037862 0.39447215
-    # 표준편차 0.18141639 0.1738924 0.16310532
-    # 224 x 224 일 때
-    # 평균 0.4330423 0.40345937 0.3941526
-    # 표준편차 0.177745 0.1705442 0.16002317
+    # 평균 0.43322375 0.40371937 0.39443374
+    # 표준편차 0.18148199 0.17403568 0.16333582
 
+    ## set seed
+    seed_torch(42)
+    
     ## save path
     args['save_path'] = './exp/' + args['mode'] + '_lr' + str(args['lr']) + '_wd' + str(args['wd']) + '_e' + str(args['epochs'])
+    if args['SAM']:
+        args['save_path'] += 'SAM'
     if not os.path.exists(args['save_path']):
         os.mkdir(args['save_path'])
     with open(args['save_path'] + '/args.pkl', 'wb') as f:
@@ -202,9 +206,6 @@ def main():
         stat_dict.pop('head.bias')
     elif args['mode'] == 'IBOT':
         stat_dict = stat_dict['state_dict']
-    # for key in list(stat_dict['state_dict'].keys()):
-    #     stat_dict['state_dict'][key.replace('module.small_encoder.', '')] = stat_dict['state_dict'].pop(key)
-    # import pdb;pdb.set_trace()
     model.load_state_dict(stat_dict, strict=False)
     model = torch.nn.DataParallel(model).cuda()
 
@@ -213,11 +214,17 @@ def main():
     if args['SAM']:
         optimizer = SAM(model.parameters(), torch.optim.AdamW, lr=args['lr'], weight_decay=args['wd'])
     else: optimizer = torch.optim.AdamW(model.parameters(), lr=args['lr'], weight_decay=args['wd'])
-    criterion = nn.CrossEntropyLoss().cuda()
+    
+    if args['FL']:
+        criterion = FocalLoss(alpha=0.25, gamma=5.0, label_smoothing=0.1)
+    else: criterion = nn.CrossEntropyLoss().cuda()
 
     ## finetune
     train_loss_list = []
     train_f1_list = []
+    val_loss_list = []
+    val_f1_list = []
+
     epochs = args['epochs']
     for epoch in range(epochs):
         start = time.time()
@@ -226,13 +233,13 @@ def main():
         train_pred = []
         train_y = []
         
-        # val_loss = 0
-        # val_pred = []
-        # val_y = []
+        val_loss = 0
+        val_pred = []
+        val_y = []
 
         model.train()
         
-        for batch in (train_loader):
+        for batch in tqdm.tqdm(train_loader):
             x, y = batch[0].cuda(), batch[1].cuda()
             
             pred = model(x)
@@ -256,45 +263,60 @@ def main():
         
         train_loss_list.append(train_loss)
         train_f1_list.append(train_f1)
-        # model.eval()
-        # with torch.no_grad():
-        #     for batch in (valid_loader):
-        #         x, y = batch[0].cuda(), batch[1].cuda()
-                
-        #         pred = model(x)
-        #         loss = criterion(pred, y)
-                
-        #         val_loss += loss.item()/len(valid_loader)
-        #         val_pred += pred.argmax(1).detach().cpu().numpy().tolist()
-        #         val_y += y.detach().cpu().numpy().tolist() 
         
-        # val_f1 = score_function(val_y, val_pred)
+        model.eval()
+        with torch.no_grad():
+            for batch in (valid_loader):
+                x, y = batch[0].cuda(), batch[1].cuda()
+                
+                pred = model(x)
+                loss = criterion(pred, y)
+                
+                val_loss += loss.item()/len(valid_loader)
+                val_pred += pred.argmax(1).detach().cpu().numpy().tolist()
+                val_y += y.detach().cpu().numpy().tolist() 
         
+        val_f1 = score_function(val_y, val_pred)
+        
+        val_loss_list.append(val_loss)
+        val_f1_list.append(val_f1)
+
         TIME = time.time() - start
         print(f'epoch : {epoch+1}/{epochs}    time : {TIME:.0f}s/{TIME*(epochs-epoch-1):.0f}s')
         print(f'TRAIN    loss : {train_loss:.5f}    f1 : {train_f1:.5f}')
-        # print(f'VALID    loss : {val_loss:.5f}    f1 : {val_f1:.5f}')
+        print(f'VALID    loss : {val_loss:.5f}    f1 : {val_f1:.5f}')
+    
     with open(args['save_path'] + '/model.pkl', 'wb') as f:
         pickle.dump(model.state_dict(), f)
-    with open(args['save_path'] + '/loss_f1.pkl', 'wb') as f:
+    with open(args['save_path'] + '/train_loss_f1.pkl', 'wb') as f:
         pickle.dump([train_loss_list, train_f1_list], f)
+    with open(args['save_path'] + '/val_loss_f1.pkl', 'wb') as f:
+        pickle.dump([val_loss_list, val_f1_list], f)
     
     ## test
     model.eval()
-    f_pred = []
+    
+    test_loss = 0
+    test_pred = []
+    test_y = []
+    
     with torch.no_grad():
         for batch in (test_loader):
-            x = batch.cuda()
+            x, y = batch[0].cuda(), batch[1].cuda()
+            
             pred = model(x)
-            f_pred.extend(pred.argmax(1).detach().cpu().numpy().tolist())
+            loss = criterion(pred, y)
+            
+            test_loss += loss.item()/len(test_loader)
+            test_pred += pred.argmax(1).detach().cpu().numpy().tolist()
+            test_y += y.detach().cpu().numpy().tolist() 
+        
+        test_f1 = score_function(test_y, test_pred)
+    print(f'TEST    loss : {test_loss:.5f}    f1 : {test_f1:.5f}')
+    # idx_to_class = {value:key for key,value in train_data.class_to_idx.items()}
+    # test_result = [idx_to_class[result] for result in test_pred]
+    with open(args['save_path'] + '/test_loss_f1.pkl', 'wb') as f:
+        pickle.dump([test_loss, test_f1], f)
     
-    # import pdb;pdb.set_trace()
-    idx_to_class = {value:key for key,value in train_data.class_to_idx.items()}
-    f_result = [idx_to_class[result] for result in f_pred]
-
-    submission = pd.read_csv("open/sample_submission.csv")
-    submission["label"] = f_result
-    submission.to_csv(args['save_path'] + '/test.csv', index=False)
-
 if __name__ == "__main__":
     main() 
